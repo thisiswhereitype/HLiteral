98a4ff6b1fc503ea85fcc69138cfb08d53ed0dab
Joe <Joe>
2017-08-04 12:42:09 +0100

    Bug fix to handle escape characters correctly

--- src/Main.hs
+++ src/Main.hs
@@ -1,44 +1,66 @@
 module Main where
 
--- #!/usr/bin/env stack
--- -- stack runghc --resolver lts-9.00
-
 import System.Environment
 import Data.List.Split(chunksOf)
 
-type Args = [String]
-
 main = do
   (path:_) <- getArgs
+
+  -- Ideally parse these from args
   let indentStr = "    "
   let lineLength = 38 - length indentStr
-  -- Handle escape chars
-  file <- lines <$> concatMap escapes <$> readFile path
-  -- Add newlines
+
+  -- Split into lines preserving newlines
+  file <- lines' <$> readFile path
   -- Chunk lines into length
-  -- let q = concatMap (chunksOf lineLength) $ map (++"\n") file
-  let q = concatMap (chunksOf lineLength) file
+  let a = (chunksOf lineLength) $ concat file
+  -- Handle escape chars
+  let b = map (concatMap escapes) file
+
   -- Convert empty lines into \n
-  -- Add backslashes to
-  let centerLines = map ((enclose indentStr) . removeEmptyLines) $ (tail . init) q
+  -- Add backslashes to enclose lines
+  let centerLines = map (enclose indentStr . removeEmptyLines) $ (tail . init) b
 
   -- Add prepending and terminating chars
-  let firstLine = [indentStr ++ "\"" ++ (head q) ++ "\\\n"]
-  let lastLine = [indentStr ++ "\\" ++ (last q) ++ "\"\n"]
+  let firstLine = [indentStr ++ "\"" ++ head b ++ "\\\n"]
+  let lastLine = [indentStr ++ "\\" ++ last b ++ "\"\n"]
   let t = (firstLine ++ centerLines ++ lastLine) :: [String]
   writeFile (path ++ ".HLiteral") $ concat t
 
+isEscape :: Char -> Bool
+isEscape c
+  | c == '\a' = True
+  | c == '\b' = True
+  | c == '\f' = True
+  | c == '\n' = True
+  | c == '\r' = True
+  | c == '\t' = True
+  | c == '\v' = True
+  | c == '\\' = True
+  | c == '\"' = True
+  | c == '\'' = True
+  | otherwise = False
+
 escapes :: Char -> String
 escapes c
+  | c == '\a' = "\\a"
+  | c == '\b' = "\\b"
+  | c == '\f' = "\\f"
+  | c == '\n' = "\\n"
+  | c == '\r' = "\\r"
+  | c == '\t' = "\\t"
+  | c == '\v' = "\\v"
   | c == '\\' = "\\\\"
   | c == '\"' = "\\\""
   | c == '\'' = "\\\'"
-  | c == '\t' = "  "
   | otherwise = [c]
 
 removeEmptyLines :: String -> String
-removeEmptyLines "" = "\n"
+removeEmptyLines "" = "\\n"
 removeEmptyLines a = a
 
 enclose :: String -> String -> String
 enclose indentStr str = indentStr ++ "\\" ++ str ++ "\\\n"
+
+lines' :: String -> [String]
+lines' str = (map (++"\n") $ init (lines str)) ++ [last (lines str)]
